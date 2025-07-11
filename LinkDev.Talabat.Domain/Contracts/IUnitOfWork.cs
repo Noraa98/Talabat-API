cb441cc9a3c7df9e1cc0cf55da5f5009ed3b931a
Noraa98 <badr91780@gmail.com>
2025-07-11 14:38:48 +0200

    Part 12 Unit Of Work

---
+++ LinkDev.Talabat.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Domain.Contracts
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IGenericRepository<Product, int> ProductRepository { get; }
+        public IGenericRepository<ProductBrand, int> BrandsRepository { get;}
+        public IGenericRepository<ProductCategory, int> CategoriesRepository { get;}
+
+        Task<int> CompleteAsync();
+    }
+    
+}
